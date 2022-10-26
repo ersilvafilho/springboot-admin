3a1c3c80f906567e8913a4a9e535c542283f2f7e
Edson Régis <ersilvafilho@gmail.com>
2022-10-25 20:34:50 -0400

    starting springboot-admin repo

---
+++ src/main/java/br/com/outlier/springadmin/SpringBootAdminApp.java
@@ -0,0 +1,17 @@
+package br.com.outlier.springadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApp.class, args);
+	}
+}
